185fe51c9356d53a78c4e3e2afc6090e34ef0364
Fr4nk1in <fushen@mail.ustc.edu.cn>
2023-02-08 18:46:11 +0800

    Remove nvim config

--- config/nvim/init.lua
+++
@@ -1,3 +0,0 @@
-require("settings")
-require("mappings")
-require("packages")
--- config/nvim/lua/mappings/init.lua
+++
@@ -1,5 +0,0 @@
--- Default <leader> key: <space>
-vim.g.mapleader = " "
-
--- Keymap section
-local map = require("helper.mapping")
